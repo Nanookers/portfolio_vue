006e72912290bfd18a9a2524df69940083753a38
Connor Clark <connordeclark@gmail.com>
2023-07-27 12:52:12 -0500

    Added some reflexive elements to the card

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -1,21 +1,3 @@
-<script lang="ts">
-  export default {
-    methods: {
-      say: async function (address: string) {
-        console.log(address)
-        try{
-            await navigator.clipboard.writeText(address)
-            alert('Copied Email')
-        } catch{
-            alert('Cannot Copy')
-        }
-
-        // Next step is to figure out how to copy to clip board
-      },
-    },
-  };
-
-</script>
 <template>
     <div class="card">
         <div class="profileImage">
@@ -25,16 +7,18 @@
             <div class="titles">
                 <h1>Connor Clark</h1>
                     <h3>Full Stack Developer | Aspring Dungeon Master</h3>
-                    <p>A full stack developer who doesn't rely on lucky skill checks. Lorem Ipsum List this</p>
+                    <p>A full stack developer who doesn't rely on lucky skill checks. Lorem Ipsum this bullshit!</p>
             </div>
             <div class="externalLinks">
-                <a href="https://www.linkedin.com/in/connor-clark-7bb39919b/" target="_blank">
+                <a href="https://www.linkedin.com/in/connor-clark-7bb39919b/" target="_blank" 
+                    title="https://www.linkedin.com/in/connor-clark-7bb39919b/">
                     <i style='font-size:30px' class='fab' id="linkedIn">&#xf0e1;</i>
                 </a>
                 
-                <i v-on:click="say('connordeclark@gmail.com')" style='font-size:32px' class='material-icons' id="email">&#xe0be;</i>
+                <i v-on:click="say('connordeclark@gmail.com')" style='font-size:32px' 
+                    class='material-icons' id="email" title="connordeclark@gmail.com">&#xe0be;</i>
 
-                <a href="https://github.com/Nanookers" target="_blank">
+                <a href="https://github.com/Nanookers" target="_blank" title="https://github.com/Nanookers">
                     <i style='font-size:30px' class='fab' id="gitHub">&#xf092;</i>
                 </a>
             </div>
@@ -42,52 +26,94 @@
     </div>
 </template>
 
+<script lang="ts">
+  export default {
+    methods: {
+    // Following function is used to copy my email to a clipboard. 
+      say: async function (address: string) {
+        console.log(address)
+        try{
+            await navigator.clipboard.writeText(address)
+            alert('Copied Email')
+        } catch{
+            alert('Cannot Copy')
+        }
+      },
+    },
+  };
+
+</script>
+
 <style scoped>
-.card{
+.card {
     display: flex;
-  
+    max-width: 650px; /* Use max-width instead of width */
+    padding: 7px;
 }
-.externalLinks{
+
+/* Use flex-wrap to wrap the links when there's not enough space */
+.externalLinks {
     display: flex;
-    justify-content: end;
+    justify-content: flex-end; /* Change 'end' to 'flex-end' for better browser support */
+    transform: translateY(65%);
 }
-.fab, .material-icons{
+
+.fab,
+.material-icons {
     cursor: pointer;
-    margin-right: 15px;
+    margin-right: 12px;
 }
+
 .profileImage {
-  border-radius: 50%;
-  overflow: hidden;
-  width: 175px;
-  height: 175px;
+    border-radius: 50%;
+    overflow: hidden;
+    width: 175px;
+    height: 175px;
 }
+
 .profileImage img {
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 }
 
-.titles{
+.titles {
     margin-left: 1rem;
-    height: 125px;
-    width: 450px;
+    width: 100%; /* Remove the fixed width to allow responsive adjustments */
+    max-width: 450px; /* Use max-width instead of width */
+    padding-bottom: 5px;
 }
 
-.titles h1{
+.titles h1 {
     margin-bottom: 10px;
     margin-top: 10px;
 }
 
-.titles h3{
+.titles h3 {
     margin: 0;
     padding: 0;
 }
-.titles p{
+
+.titles p {
     margin: 0;
-    padding-top: 8px ;
+    padding-top: 8px;
+    width: 100%;
+    max-width: 392px;
 }
-svg{
+
+svg {
     width: 100px;
     height: 100px;
 }
 
+/* Add a media query for smaller screens */
+@media (max-width: 768px) {
+    .card {
+        flex-direction: column; /* Stack the content in a column */
+        align-items: center; /* Center the items in the card */
+    }
+    .externalLinks {
+        justify-content: center; /* Center the links in smaller screens */
+        transform: none; /* Remove the translateY */
+    }
+}
 </style>
