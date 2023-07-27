ecfd13fc49b6ea70cee086735cfedc34fe262701
Connor Clark <connordeclark@gmail.com>
2023-07-27 16:54:51 -0500

    Building a twenty sided die in css | 4 sides

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
   import Badge from '../src/components/Badge.vue'
+  import Dice from '../src/components/Dice.vue'
 </script>
 
 <template>
-  <Badge /> 
+  <!-- navbar -->
+  <!-- resume official / resume Character sheet -->
+  <!-- Cover Letter official / cover letter in chartacter sheet-->
+  <!-- Dice roll? -->
+  <!-- <Badge />  -->
+  <div class="diceContainer"><Dice /></div>
+  
 </template>
 
 <style scoped>
-
+.diceContainer {
+  display: flex;
+  justify-content: center; /* Center horizontally */
+  align-items: center; /* Center vertically */
+  height: 100vh; /* Set the container to full viewport height */
+}
 </style>
---
+++ src/components/Dice.vue
@@ -0,0 +1,50 @@
+<template>
+<!-- Set up divs for the dice each side will have  -->
+    <div class="skillCheck">
+        <div class="icosahedron">
+            <div class="dieFace"></div>
+            <div class="dieFace"></div>
+            <div class="dieFace"></div>
+            <div class="dieFace"></div>
+        </div>
+        <div id="roll" class="rollButton">
+            <!-- <button>Check Skill!</button> -->
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+.icosahedron {
+  width: 0;
+  height: 0;
+  position: relative;
+  transform-style: preserve-3d;
+}
+.dieFace{
+  position: absolute;
+  width: 100px; /* Adjust the size of the triangles as needed */
+  height: 100px;
+  background-color: #ccc; /* Adjust the colors and styles as needed */
+  clip-path: polygon(50% 0%, 100% 100%, 0% 100%); /* Create an equilateral triangle */
+  transform-origin: 50% 100%;
+}
+.dieFace:nth-child(1) {
+  transform: rotateY(0deg) rotateX(35.264deg) translateZ(85.56px);
+}
+
+.dieFace:nth-child(2) {
+  transform:  rotateY(30deg) rotateX(35.264deg) rotateZ(76deg) translateZ(169.56px) translateX(-3px) translateY(52px);
+}
+
+.dieFace:nth-child(3) {
+  transform: rotateY(-30deg) rotateX(35.264deg) rotateZ(284deg) translateZ(85.56px) translateX(47px) translateY(-19px);
+}
+.dieFace:nth-child(4) {
+  transform: rotateY(0deg) rotateX(147deg) translateZ(85.56px)  translateY(52px);
+}
+
+</style>
